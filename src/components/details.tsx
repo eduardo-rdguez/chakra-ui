8eafa3121cf81adf05398595cac0ee9c110edaf0
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-05-03 22:21:35 -0500

    Implement responsive design in components

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -10,9 +10,12 @@ import {
   Select,
   Checkbox,
   Button,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 
 const Details = () => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 });
+
   return (
     <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
       <VStack spacing={3} alignItems="flex-start">
@@ -20,13 +23,13 @@ const Details = () => {
         <Text>If you alredy have an account, please login.</Text>
       </VStack>
       <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>First Name</FormLabel>
             <Input placeholder="John" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>Last Name</FormLabel>
             <Input placeholder="Doe" />
@@ -38,7 +41,7 @@ const Details = () => {
             <Input placeholder="Blvd. Broken Dreams 21" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>City</FormLabel>
             <Input placeholder="San Francisco" />
