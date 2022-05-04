412190a7d8415b76dc3d9bd1efeb077a95199324
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-05-03 21:56:39 -0500

    Update details component title

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -16,7 +16,7 @@ const Details = () => {
   return (
     <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
       <VStack spacing={3} alignItems="flex-start">
-        <Heading size="2xl">Your Details</Heading>
+        <Heading size="2xl">Your details</Heading>
         <Text>If you alredy have an account, please login.</Text>
       </VStack>
       <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
